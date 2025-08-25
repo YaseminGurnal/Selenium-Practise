c5435fe64145874d582df6cbb20aae41b4fafb47
YaseminGurnal <ysmngurnal@gmail.com>
2025-08-25 18:35:04 +0300

    tüm dosyalar

--- src/Utility/BaseDriver.java
+++ src/Utility/BaseDriver.java
@@ -1,2 +1,14 @@
-package Utility;public class BaseDriver {
+package Utility;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseDriver {
+    public static WebDriver driver;
+
+    static {
+
+        driver=new ChromeDriver();
+    }
+
 }
